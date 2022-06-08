1f40223eda021634780ba157957136737564c85b
ABDULRAZZAK ALSSSOUKI <snowstarinthesky@gmail.com>
2022-06-08 19:07:35 +0200

    Removing wrong importing

--- Weather/src/App.vue
+++ Weather/src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { RouterView } from "vue-router";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/fontawesome-svg-icons";
 </script>
 
 <template>
